bbfa0c8f9aec4b7db914e30a209e6a32bb49e7f0
Paige-Peterson <ioptio@riseup.net>
2015-07-17 19:05:53 -0700

    clean up navigation, create specialised side navs

--- app/js/main.js
+++ app/js/main.js
@@ -1,22 +1,35 @@
 /* jshint undef: false*/
-var fixNavResize = function() {
+var savedState;
+var fixNavResize = function(check) {
   /*jshint camelcase: false */
   /*jscs:disable requireCamelCaseOrUpperCaseIdentifiers */
-  if (Foundation.utils.is_medium_up() === true) {
+  if (Foundation.utils.is_medium_up() === true && (check === 'ready' || savedState === 1)) {
     $('header .has-dropdown-holder').removeClass('has-dropdown');
     $('header .dropdown-holder').removeClass('dropdown');
     $('header nav').addClass('not-mobile');
     $('header .build-logo img').css({ 'left': 'auto' });
-  } else if (Foundation.utils.is_small_only() === true) {
+    $('footer .top li').css({ 'padding-bottom': '0'});
+    $('footer .top li.homepage-link').css({ 'position': 'static', 'top': 'auto', 'left': 'auto%', 'margin-left': 'auto'});
+    $('footer .top li.community-link').css({ 'margin-top': 'auto' });
+    $('footer .top li.meetup-link').css({ 'margin-top': 'auto' });
+    $('footer .top li.twitter-link').css({ 'margin-top': 'auto' });
+    savedState = 0;
+  } else if (Foundation.utils.is_small_only() === true && (check === 'ready' || savedState === 0)) {
     $('header .has-dropdown-holder').addClass('has-dropdown');
     $('header .dropdown-holder').addClass('dropdown');
     $('header nav').removeClass('not-mobile');
     $('header .build-logo img').css({ 'left': '15px' });
+    $('footer .top li').css({ 'padding-bottom': '20px'});
+    $('footer .top li.homepage-link').css({ 'position': 'absolute', 'top': '30px', 'left': '50%', 'margin-left': '-25%'});
+    $('footer .top li.community-link').css({ 'margin-top': '100px' });
+    $('footer .top li.meetup-link').css({ 'margin-top': '99px' });
+    $('footer .top li.twitter-link').css({ 'margin-top': '-140px' });
+    savedState = 1;
   }
 };
 $('document').ready(function() {
   $('header li.parent-link a').contents().unwrap();
-  fixNavResize();
+  fixNavResize('ready');
 });
 $(window).resize(function() {
   fixNavResize();
