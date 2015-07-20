9a2e7055c14caffa00bf95d71ff33cde877165ce
Paige-Peterson <ioptio@riseup.net>
2015-07-20 10:56:23 -0700

    fix broken links, js long line warnings

--- app/js/main.js
+++ app/js/main.js
@@ -8,8 +8,12 @@ var fixNavResize = function(check) {
     $('header .dropdown-holder').removeClass('dropdown');
     $('header nav').addClass('not-mobile');
     $('header .build-logo img').css({ 'left': 'auto' });
-    $('footer .top li').css({ 'padding-bottom': '0'});
-    $('footer .top li.homepage-link').css({ 'position': 'static', 'top': 'auto', 'left': 'auto%', 'margin-left': 'auto'});
+    $('footer .top li').css({ 'padding-bottom': '0' });
+    $('footer .top li.homepage-link').css({
+      'position': 'static',
+      'top': 'auto',
+      'left': 'auto%',
+      'margin-left': 'auto' });
     $('footer .top li.community-link').css({ 'margin-top': 'auto' });
     $('footer .top li.meetup-link').css({ 'margin-top': 'auto' });
     $('footer .top li.twitter-link').css({ 'margin-top': 'auto' });
@@ -19,8 +23,12 @@ var fixNavResize = function(check) {
     $('header .dropdown-holder').addClass('dropdown');
     $('header nav').removeClass('not-mobile');
     $('header .build-logo img').css({ 'left': '15px' });
-    $('footer .top li').css({ 'padding-bottom': '20px'});
-    $('footer .top li.homepage-link').css({ 'position': 'absolute', 'top': '30px', 'left': '50%', 'margin-left': '-25%'});
+    $('footer .top li').css({ 'padding-bottom': '20px' });
+    $('footer .top li.homepage-link').css({
+      'position': 'absolute',
+      'top': '30px',
+      'left': '50%',
+      'margin-left': '-25%' });
     $('footer .top li.community-link').css({ 'margin-top': '100px' });
     $('footer .top li.meetup-link').css({ 'margin-top': '99px' });
     $('footer .top li.twitter-link').css({ 'margin-top': '-140px' });
